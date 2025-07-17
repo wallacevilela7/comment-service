7b80e38789fcf00f8674d6c027a185c47a9caf0c
wallacevilela7 <wallace.vilela9@gmail.com>
2025-07-17 11:11:34 -0300

    Setup comment service microservice

---
+++ comments-ms/src/main/java/tech/wvs/commentsms/CommentsMsApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.commentsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentsMsApplication.class, args);
+    }
+
+}
